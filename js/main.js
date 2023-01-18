a2712e2100f0abf52ed9e9403905ef3ec2916911
Kirill Kolchev <kirkol4evv@gmail.com>
2023-01-18 10:18:14 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.slaider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    variableWidth: true,
+    centerMode: true,
+    centerPadding: '50px',
+    slidesToShow: 1,
+    focusOnSelect:false,
+})
+
